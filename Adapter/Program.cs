88ebe4f33e8a1eb6a23ad337d239b2bf2be7da79
Arnaud Bagbila <arbagb@outlook.com>
2023-07-04 21:41:10 +0100

    Adapter logger

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.IO;
 
 namespace Adapter
 {
@@ -6,7 +9,31 @@ namespace Adapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logger = new FileLogger<Program>("log.txt");
+            logger.LogDebug("This is a new message");
+        }
+
+        class FileLogger<T> : FileStream, ILogger<T>
+        {
+            public FileLogger(string path) : base(path, FileMode.Append) { }
+            public IDisposable BeginScope<TState>(TState state) where TState : notnull
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                byte[] messageByteArray = new UTF8Encoding(true).GetBytes(state.ToString() + "\n");
+                Write(messageByteArray, 0, messageByteArray.Length);
+
+                Write(messageByteArray, 0, messageByteArray.Length);
+                Flush();
+            }
         }
     }
 }
